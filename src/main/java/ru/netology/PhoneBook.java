49dad5f6a7035926915e759b2536a3ec5bcdcd28
Usenina Maria <mariyausenina@gmail.com>
2023-11-02 20:31:28 +0400

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,21 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> contacts;
+
+    public PhoneBook() {
+        contacts = new HashMap<>();
+    }
     public int add(String name, String phoneNumber) {
-        return 0;
+        if (contacts.containsKey(name)) {
+            return contacts.size();
+        }
+        else {
+            contacts.put(name,phoneNumber);
+            return contacts.size();
+        }
     }
 }
